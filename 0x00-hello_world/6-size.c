0b4839cda6d02fd9ea8c2b5d8a3489faea545327
nada445 <nadaelsharkawy545@gmail.com>
2023-08-10 16:50:25 -0700

    fixed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %d", sizeof(char));
+printf(" byte(s) \n");
+printf("Size of an int: %d", sizeof(int));
+printf(" byte(s) \n");
+printf("Size of a long int: %d", sizeof(long int));
+printf(" byte(s)\n");
+printf("Size of a long long int: %d", sizeof(long long int));
+printf(" byte(s)\n");
+printf("Size of a float: %d", sizeof(float));
+printf(" byte(s)\n");
+return (0);
+}
