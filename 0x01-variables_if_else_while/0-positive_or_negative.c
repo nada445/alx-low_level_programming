110eeb957aeae45deb8d9c333ca254b2d85b3839
nada445 <nadaelsharkawy545@gmail.com>
2023-08-11 09:44:55 -0700

    g

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%u is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%u is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
-printf("%u is negative\n", n);
+printf("%d is negative\n", n);
 return (0);
 }
