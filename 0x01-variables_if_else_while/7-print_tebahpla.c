d868a49816e472f81aef02f2c7d1dea3786777b0
nada445 <nadaelsharkawy545@gmail.com>
2023-08-11 11:39:41 -0700

    v

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char h = 122;
+while (h != 96)
+{
+putchar(h);
+h--;
+}
+
+putchar('\n');
+return (0);
+}
