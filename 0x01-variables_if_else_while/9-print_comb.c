423b7f24b378d8d7fe037d9ae6ac139cfdefd46a
nada445 <nadaelsharkawy545@gmail.com>
2023-08-11 11:53:45 -0700

    v

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar('0' + i);
+putchar(',');
+putchar(' ');
+i++;
+}
+putchar('\n');
+return (0);
+}
