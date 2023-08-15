864523d3c1187a14b48af91338e232de3bea8c31
nada445 <nadaelsharkawy545@gmail.com>
2023-08-15 16:40:38 -0700

    g

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,37 +13,35 @@ void print_to_98(int n)
 int i;
 if (n < 98)
 {
+if (n < 0)
+n *= -1;
 for (i = n; i < 99; i++)
 {
-if (i > 10)
+if (i >= 10)
 {
 _putchar('0' + i / 10);
-_putchar('0' + i % 10);
-}
+_putchar('0' + i % 10); }
 else
-{
 _putchar('0' + i);
-}
 if (i != 98)
 {
 _putchar(',');
-_putchar(' ');
-}
-}
-}
+_putchar(' '); }}}
 else
 {
-for (i = n; i > 97; i--)
+for (i = 98; i <= n; i++)
+{
+if (i >= 100)
+{
+_putchar('0' + i / 100);
+_putchar('0' + (i % 100) / 10);
+_putchar('0' + (i % 100) % 10); }
+else
 {
 _putchar('0' + i / 10);
-_putchar('0' + i % 10);
+_putchar('0' + i % 10); }
 if (i != 98)
 {
 _putchar(',');
-_putchar(' ');
-}
-}
-}
-
-_putchar('\n');
-}
+_putchar(' '); }}}
+_putchar('\n'); }
