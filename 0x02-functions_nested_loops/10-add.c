dfeb5ef4df3a8732a6202bcf6cc71a783a88100e
nada445 <nadaelsharkawy545@gmail.com>
2023-08-15 16:02:14 -0700

    g

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * add - adds 2 numbers and prints the result
+ * @a : first number
+ * @b : second number
+ * description: summition
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
