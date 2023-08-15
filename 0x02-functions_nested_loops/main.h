f87a0b7950e2a8289784132b0326b107cc5a22f8
nada445 <nadaelsharkawy545@gmail.com>
2023-08-15 12:45:53 -0700

    g

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,10 +6,12 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * print_alphabet prints the alphabet in lowercase
+ * print_alphabet_x10 prints the alphabet 10 times
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif /** MAIN_H_INCLUDED*/
