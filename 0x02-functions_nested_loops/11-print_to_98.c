2cb4ecdea422e4b60bd8ad360756d29ddaaa0e2c
nada445 <nadaelsharkawy545@gmail.com>
2023-08-15 16:53:56 -0700

    g

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,21 +6,19 @@
  * print_to_98 - prints all natural numbers from n to 98, followed by new line
  * @n: a number to start count from
  * description: count to 98
- * Return: Always 0 (Success)
+ * Return: Alwas 0 (Success)
  */
 void print_to_98(int n)
 {
-int i, tmp = n;
+int i;
 if (n < 98)
 {
-if (n < 0)
-n *= -1;
 for (i = n; i < 99; i++)
 {
-if (tmp < 0)
-_putchar('-');
-if (i >= 10)
+if (i >= 10 || i * -1 >= 10)
 {
+if (i < 10)
+_putchar('-');
 _putchar('0' + i / 10);
 _putchar('0' + i % 10); }
 else
@@ -31,13 +29,14 @@ _putchar(',');
 _putchar(' '); }}}
 else
 {
-for (i = 98; i <= n; i++)
+for (i = n; i > 97; i--)
 {
 if (i >= 100)
 {
 _putchar('0' + i / 100);
 _putchar('0' + (i % 100) / 10);
-_putchar('0' + (i % 100) % 10); }
+_putchar('0' + (i % 100) % 10);
+}
 else
 {
 _putchar('0' + i / 10);
