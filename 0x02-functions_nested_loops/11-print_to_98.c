74ba2b67a8de4180b0b5ab1fc9cc2eacc46f2086
nada445 <nadaelsharkawy545@gmail.com>
2023-08-15 16:22:45 -0700

    count

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,6 +11,8 @@
 void print_to_98(int n)
 {
 int i;
+if (n < 98)
+{
 for (i = n; i < 99; i++)
 {
 if (i > 10)
@@ -28,5 +30,20 @@ _putchar(',');
 _putchar(' ');
 }
 }
+}
+else
+{
+for (i = n; i > 97; i--)
+{
+_putchar('0' + i / 10);
+_putchar('0' + i % 10);
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+
 _putchar('\n');
 }
