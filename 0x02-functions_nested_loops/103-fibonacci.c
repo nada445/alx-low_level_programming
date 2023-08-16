d4cb5217a703455c0c456fc1b0100cf35d3d96bf
nada445 <nadaelsharkawy545@gmail.com>
2023-08-15 20:00:17 -0700

    gg

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_Fibonacci_numbers - print_Fibonacci_numbers
+ * description: print_Fibonacci_numbers
+ * Return: Always 0 (Success)
+ */
+
+void print_Fibonacci_numbers(void)
+{
+int i;
+int arr[50];
+arr[0] = 1;
+arr[1] = 2;
+for (i = 2; i < 32; i++)
+{
+arr[i] = arr[i - 1] + arr[i - 2];
+}
+for (i = 0; i < 32; i++)
+{
+if (arr[i] % 2 == 0)
+{printf("%d", arr[i]);
+if (i != 31)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+}
+/**
+ * main - entry oint
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_Fibonacci_numbers();
+return (0);
+}
