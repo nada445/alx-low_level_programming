fb624e0063568357e0315721e9d29893551b6a9c
nada445 <nadaelsharkawy545@gmail.com>
2023-08-16 14:59:39 -0700

    gg

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,9 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 	{
-		if (month == 2 && day == 60)
-		day = day;
-		else if (month > 2 && day > 60)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
