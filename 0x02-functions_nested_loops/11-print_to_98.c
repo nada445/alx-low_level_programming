525e6d050f8e0190e1b180e3302a3db2a48decb3
nada445 <nadaelsharkawy545@gmail.com>
2023-08-15 17:06:41 -0700

    g

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,9 +15,9 @@ if (n < 98)
 {
 for (i = n; i < 99; i++)
 {
+tmp = i;
 if (i < 0)
 {
-tmp = i;
 _putchar('-');
 i *= -1; }
 if (i >= 10 || i * -1 >= 10)
@@ -26,7 +26,7 @@ _putchar('0' + i / 10);
 _putchar('0' + i % 10); }
 else
 _putchar('0' + i);
-if (i != 98)
+if (tmp != 98)
 {
 _putchar(',');
 _putchar(' '); }
