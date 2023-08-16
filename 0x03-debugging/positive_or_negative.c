37bea1516b844b7bf2c845d1c324eb309a3a8926
nada445 <nadaelsharkawy545@gmail.com>
2023-08-16 11:13:09 -0700

    gfg

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * positive_or_negative - check sign
+ * @i: number to check
+ * description: fkrfkfkf
+ */
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+printf("%d is negative\n", i);
+}
