83d01c5b4656090d42a48d1ba76717c0483bacb5
nada445 <nadaelsharkawy545@gmail.com>
2023-08-15 21:03:36 -0700

    f

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_Fibonacci_numbers - print_Fibonacci_numbers
+ * description: multipiles of 3 and 5
+ * Return: Always 0 (Success)
+ */
+
+void print_Fibonacci_numbers(void)
+{
+long double i3;
+long double i2 = 2.0;
+long double i1 = 1.0;
+int counter = 2;
+printf("%d, ", 1);
+printf("%d, ", 2);
+while (counter < 98)
+{
+i3 = i2 + i1;
+printf("%.0Lf", i2 + i1);
+if (counter != 97)
+printf(", ");
+long double tmp = i2;
+i2 = i3;
+i1 = tmp;
+counter++;
+}
+}
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_Fibonacci_numbers();
+return (0);
+}
