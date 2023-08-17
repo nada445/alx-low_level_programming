d6e15ca63ce34c6086bb1997da0c573e5fe20598
nada445 <nadaelsharkawy545@gmail.com>
2023-08-17 16:38:29 -0700

    lasr tru

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,45 @@
  */
 void print_number(int n)
 {
-unsigned int tmp;
-tmp = n;
+int i;
+int countr;
+unsigned int x;
+int tmp;
+x = n;
+if (n > 2147483647)
+return;
 if (n < 0)
 {
-_putchar('-');
-tmp = -1 * tmp;
+_putchar(45);
+x = -1 * x;
 }
-if (tmp / 10 > 0)
-print_number(tmp / 10);
-_putchar('0' + tmp % 10);
+countr = -1;
+tmp = n;
+while (tmp != 0)
+{
+countr++;
+tmp /= 10;
+}
+for (i = _pow(10, countr); i != 0; i /= 10)
+{
+_putchar('0' + x / i);
+x %= i;
+}
+}
+/**
+ * _pow - ggg
+ * @x: fff
+ * @y: ffff
+ * Return: gg
+ */
+int _pow(int x, int y)
+{
+int i;
+int result;
+result = 1;
+for (i = 0; i < y; i++)
+{
+result *= x;
+}
+return (result);
 }
