8c80287d0d46183efd5141306071d6d502a6ff94
nada445 <nadaelsharkawy545@gmail.com>
2023-08-17 07:55:44 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: program to print numbers except for 3 and 5 multiples
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+			printf("fizz");
+		if (i % 5 == 0)
+			printf("buzz");
+		if ((i % 3 != 0) && (i % 5 != 0))
+			printf("%d", i);
+		if (i != 100)
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
