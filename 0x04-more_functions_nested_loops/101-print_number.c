fbedc7e1388bf801ce7976a6bdd6ef566c8b4c5c
nada445 <nadaelsharkawy545@gmail.com>
2023-08-17 14:58:58 -0700

    gg

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,38 +8,14 @@
  */
 void print_number(int n)
 {
-int i;
-int j;
-int countr;
-int flag = 0;
-int tmp = 10;
-if (n > 2147483647)
-return;
+unsigned int num;
+num = n;
 if (n < 0)
 {
 _putchar(45);
-n = -1 * n;
+num = -1 * num;
 }
-if (n < 10)
-_putchar('0' + n);
-else
-{
-for (j = 1000000000; flag != 1; j /= 10)
-{
-if (n >= j)
-{
-flag = 1;
-countr = 0;
-for (i = j; countr < tmp  ; i /= 10)
-{
-countr++;
-_putchar('0' + n / i);
-n %= i;
-}
-}
-tmp--;
-if (flag)
-break;
-}
-}
+if (num / 10 > 0)
+print_number(num / 10);
+_putchar('0' + num % 10);
 }
