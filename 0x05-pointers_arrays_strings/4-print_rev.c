26dcee76884624f09e09cdb07e3f315a25a85582
nada445 <nadaelsharkawy545@gmail.com>
2023-08-21 13:08:11 -0700

    reverse the str

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#include "main.h"
+/**
+ * _strlen - prints out the length of the str
+ * @s: str to print its size
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int i;
+int counter;
+counter = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+counter++;
+}
+
+return (counter);
+}
+
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line
+ * @s: the string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i;
+
+for (i = 0; i < _strlen(s); i++)
+{
+_putchar(s[_strlen(s) - 1 - i]);
+}
+_putchar('\n');
+}
