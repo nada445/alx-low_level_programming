9106a0f0302a92bae4080936be56bd73b19255c1
nada445 <nadaelsharkawy545@gmail.com>
2023-08-21 14:52:48 -0700

    am the one who decides how many number would be printed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,27 +1,7 @@
 #include "main.h"
-/**
- * _strlen - prints out the length of the str
- * @s: str to print its size
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-int i;
-int counter;
-
-counter = 0;
-
-for (i = 0; s[i] != '\0'; i++)
-{
-counter++;
-}
-
-return (counter);
-}
 
 /**
- * _print_array - prints n elements of an arr of int followed by a new line
+ * print_array - prints n elements of an arr of int followed by a new line
  * @a: ar
  * @n: number of elements
  * Return: void
@@ -32,7 +12,7 @@ void print_array(int *a, int n)
 int i;
 
 if (n < 0)
-return;
+n = 0;
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);
