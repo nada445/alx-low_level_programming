f9397ddfc67fec9fbc36ff400d458f81deb87d67
nada445 <nadaelsharkawy545@gmail.com>
2023-08-23 15:51:57 -0700

    copying string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -32,10 +32,12 @@ int k;
 s = _strlen(src);
 k = _strlen(dest);
 
-for (i = 0; (i < n && i < s && i < k); i++)
+for (i = 0; (i < n && src[i] != '\0'); i++)
 {
 dest[i] = src[i];
 }
+for (; i < n; i++)
+dest[i] = '\0';
 return (dest);
 
 }
