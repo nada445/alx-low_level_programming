9f956d1d13dc6d4534cdb8fa94aab7fc3c1dd955
nada445 <nadaelsharkawy545@gmail.com>
2023-08-28 15:07:13 -0700

    l

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,16 @@
 
 char *_strchr(char *s, char c)
 {
+
 int i;
 
-i = 0;
 
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; *(s + i) >= '\0'; i++)
 {
-if (s[i] == c)
-break;
-}
+if (*(s + i) == c)
+{
 return (s + i);
+}
+}
+return (NULL);
 }
