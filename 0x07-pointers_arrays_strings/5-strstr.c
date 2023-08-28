4234615b21fdcf7ed9fae95f337049bbfdfbaa70
nada445 <nadaelsharkawy545@gmail.com>
2023-08-28 16:11:08 -0700

    gg

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strpbrk -  locates a substring.
+ * _strstr -  locates a substring.
  * @haystack: pointer (memory block to scan)
  * @needle: pointer (memory block reference)
  * Return:  pointer to the byte in s that matches one of the bytes in accept,
@@ -38,6 +38,8 @@ istrt++;
 }
 if (flag == 1 && n == sz)
 return (haystack + i);
+else
+flag = 0;
 }
 }
 return (NULL);
