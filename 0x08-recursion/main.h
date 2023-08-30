9ec95658d3beb574a35d0672c186a6094277ffee
nada445 <nadaelsharkawy545@gmail.com>
2023-08-30 12:43:04 -0700

    h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,7 +15,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-
+int _sqrt(int n,int guess);
 
 
 #endif /* MAIN_H_INCLUDED */
