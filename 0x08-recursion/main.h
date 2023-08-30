e0e3e9c8b29539b41310f1b4d112b823645bc43d
nada445 <nadaelsharkawy545@gmail.com>
2023-08-30 13:29:29 -0700

    dd

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -16,6 +16,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n,int guess);
-
-
+int is_prime_number(int n);
+int prim(int n,int i);
 #endif /* MAIN_H_INCLUDED */
