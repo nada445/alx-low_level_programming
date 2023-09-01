301945c98293b4e057478b1f15a00cae279ad0d8
nada445 <nadaelsharkawy545@gmail.com>
2023-09-01 11:20:35 -0700

    my name is

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c);
+
+
+#endif /* MAIN_H_INCLUDED */
