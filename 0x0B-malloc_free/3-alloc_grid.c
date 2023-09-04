56fe7438cce9f5f23fa24321de77e105b2de2a19
nada445 <nadaelsharkawy545@gmail.com>
2023-09-04 12:44:32 -0700

    2d memalloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - 2darr
+ * @width: cols
+ * @height: rows
+ * Return: 2darray
+ */
+
+int **alloc_grid(int width, int height)
+{
+int i;
+int j;
+int **arr;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+arr = (int **)malloc(height * sizeof(int));
+
+if (arr == NULL)
+return (NULL);
+
+for (i = 0 ; i < height; i++)
+arr[i] = (int *)malloc(width * sizeof(int));
+
+if (arr == NULL)
+return (NULL);
+
+for (i = 0; i < height ; i++)
+{
+for (j = 0; j < width; j++)
+arr[i][j] = 0;
+}
+return (arr);
+
+}
