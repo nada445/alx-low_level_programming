dc332ec3f8583d31b6c0553b020901573f062aa4
nada445 <nadaelsharkawy545@gmail.com>
2023-09-05 19:22:17 -0700

    g

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,80 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - prints out the length of the str
+ * @s: str to print its size
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
+/**
+ * strtow - splits a string into words
+ * @str: str
+ * Return: pointer to array of strings
+ */
+char **strtow(char *str)
+{
+int j;
+int i;
+char **arr;
+int wordindx[100];
+int cntr;
+int spccntr;
+int idx;
+cntr = 0;
+spccntr = 0;
+idx = 0;
+if (str == NULL || *str == '\0')
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] != ' ' && ((str[i - 1] == ' ' && i !=0) || i == 0))
+{
+wordindx[cntr] = i;
+cntr++;
+}
+}
+arr = (char **)malloc((cntr) * sizeof(char *));
+if(arr == NULL)
+{
+free(arr);
+return (NULL);
+}
+for (i = 0; i < cntr; i++)
+{
+spccntr = 0;
+for (j = wordindx[i]; str[j] != ' '; j++)
+spccntr++;
+arr[i] =(char *)malloc(spccntr);
+}
+for (i = 0; i < cntr; i++)
+{
+if (arr[i] == NULL)
+{
+for (i = 0; i < cntr; i++)
+free(arr[i]);
+free(arr);
+return (NULL);
+}
+}
+for (i = 0; i < cntr; i++)
+{
+idx = 0;
+for (j = wordindx[i]; str[j] != ' '; j++)
+{
+arr[i][idx] = str[j];
+idx++;
+}
+arr[i][idx +1] = '\0';
+}
+arr[cntr] = NULL;
+return (arr);
+}
