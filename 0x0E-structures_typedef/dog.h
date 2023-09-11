0859a1f51c379d3efbba14ae156b229403e325ba
nada445 <nadaelsharkawy545@gmail.com>
2023-09-11 13:05:12 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,18 @@ float age;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+/**
+ * struct dog_t - Short description
+ * @name: First member
+ * @owner: Second member
+ * @age: third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog_t
+{
+char *name;
+char *owner;
+float age;
+} dog_t;
 #endif /*  _DOG_H_INCLUDED */
