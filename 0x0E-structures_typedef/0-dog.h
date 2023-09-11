7dec41ddf09e56ff28e5f9f143ffee49c930f4bb
nada445 <nadaelsharkawy545@gmail.com>
2023-09-11 12:06:59 -0700

    f

---
+++ 0x0E-structures_typedef/0-dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_INCLUDED
+#define _DOG_H_INCLUDED
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @owner: Second member
+ * @age: third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+char *owner;
+float age;
+
+};
+#endif /*  _DOG_H_INCLUDED */
