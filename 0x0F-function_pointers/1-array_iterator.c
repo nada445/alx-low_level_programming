a0c2f96dda399e149ebec1a95baba7546e60b3c2
nada445 <nadaelsharkawy545@gmail.com>
2023-09-13 15:33:15 -0700

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given
+ * @array: array
+ * @size: size
+ * @action: pointer to fun
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (action)
+{
+size_t i;
+for (i = 0; i < size; i++)
+action(array[i]);
+}
+}
