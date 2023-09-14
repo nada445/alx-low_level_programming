f8d5e4c3824b675ee3eaf0b2bf83930e8c815c85
nada445 <nadaelsharkawy545@gmail.com>
2023-09-14 14:01:38 -0700

    g

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,85 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+ * print_int - does something
+ * @ap: variable
+ * Return: void
+ */
+
+void print_int(va_list ap)
+{
+printf("%d", va_arg(ap, int));
+}
+/**
+ * print_string - does something
+ * @ap: variable
+ * Return: void
+ */
+void print_string(va_list ap)
+{
+char *str;
+str = va_arg(ap, char *);
+switch((int)(!str))
+case 1:
+str ="(nil)";
+printf("%s", str);
+}
+/**
+ * print_float - does something
+ * @ap: variable
+ * Return: void
+ */
+void print_float(va_list ap)
+{
+printf("%f", va_arg(ap, double));
+}
+/**
+ * print_char - does something
+ * @ap: variable
+ * Return: void
+ */
+void print_char(va_list ap)
+{
+printf("%c", va_arg(ap, int));
+}
+/**
+ * print_all - does something
+ * @format: variable
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+int i;
+int j;
+va_list ap;
+typ_t typs[] = {
+{"c", print_char},
+{"f", print_float},
+{"s", print_string},
+{"i", print_int},
+{NULL, NULL}
+};
+va_start(ap, format);
+j = 0;
+while (format && format[j])
+{
+i = 0;
+while (i < 4)
+{
+if(format[j] == typs[i].s[0])
+{
+typs[i].f(ap);
+if(format[j + 1])
+printf(" ,");
+}
+i++;
+}
+j++;
+}
+
+printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,9 @@
 #ifndef VARIADIC_FUNCTIONS_H_INCLUDED
 #define VARIADIC_FUNCTIONS_H_INCLUDED
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -14,5 +16,19 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+ * struct typ - Struct op
+ *
+ * @t: The operator
+ * @f: The function associated
+ */
+typedef struct typ
+{
+char *s;
+void (*f)(va_list);
+} typ_t;
+void print_string(va_list);
+void print_int(va_list);
+void print_float(va_list);
+void print_char(va_list);
 #endif /* VARIADIC_FUNCTIONS_H_INCLUDED*/
