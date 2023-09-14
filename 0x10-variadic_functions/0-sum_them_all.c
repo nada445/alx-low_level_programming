5a08e59bdead6d3b1b03c058a5e4b1363abe217b
nada445 <nadaelsharkawy545@gmail.com>
2023-09-14 11:17:51 -0700

    g

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - does something
+ * @n: variable
+ *
+ * Return: int
+ */
+ 
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int sum;
+va_list ap;
+sum = 0;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+sum += va_arg(ap,int);
+
+return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FUNCTIONS_H_INCLUDED
+#define VARIADIC_FUNCTIONS_H_INCLUDED
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H_INCLUDED*/
