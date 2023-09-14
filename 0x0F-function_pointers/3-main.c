0480c5d9e8342c48b66ff3c1ae93881fac9fafc4
nada445 <nadaelsharkawy545@gmail.com>
2023-09-13 17:48:58 -0700

    h

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: num
+ * @argv:wdc
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+char *op;
+int (*ptr)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op = argv[2];
+
+if (num2 == 0 && (op[0] == '/' || op[0] == '%'))
+{
+printf("Error\n");
+exit(100);
+}
+ptr = get_op_func(op);
+if (!ptr)
+{
+printf("Error\n");
+exit(99);
+}
+result = ptr(num1, num2);
+printf("%d\n", result);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - dsckcs
+ * @a: fvv
+ * @b: fevfv
+ * Return: evev
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - dsckcs
+ * @a: fvv
+ * @b: fevfv
+ * Return: evev
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - dsckcs
+ * @a: fvv
+ * @b: fevfv
+ * Return: evev
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - dsckcs
+ * @a: fvv
+ * @b: fevfv
+ * Return: evev
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - dsckcs
+ * @a: fvv
+ * @b: fevfv
+ * Return: evev
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
