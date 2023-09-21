528f0f594957a21314cb76e95993a81080094c92
NADA <nadaelsharkawy545@gmail.com>
2023-09-21 10:25:31 -0700

    g

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,7 +31,8 @@ int _strlen(char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
-
+	if (!new)
+		return (NULL);
 	new->str = strdup(str);
 	new->len = _strlen(new->str);
 	new->next = *head;
