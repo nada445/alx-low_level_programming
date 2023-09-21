eb3f80e1ad5c5a08dd47da9f7162b06118e662ac
NADA <nadaelsharkawy545@gmail.com>
2023-09-21 11:48:27 -0700

    a

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - dds a new node at the beginning of a list_t list
+ * @head: var
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp = head;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		free(head);
+		head = tmp;
+	}
+	free(head);
+}
