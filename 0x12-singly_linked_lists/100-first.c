df93ef4ce794606806f8e6e873e2ed7aca44cbab
NADA <nadaelsharkawy545@gmail.com>
2023-09-21 12:27:43 -0700

    a

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * Return: void
  */
 
-void before_main()
+void before_main(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, y
+ou must allow,\nI bore my house upon my back!\n");
 }
