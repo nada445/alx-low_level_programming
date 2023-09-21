e2b944c4694466a607afcbe4d5a8c055bf0c318d
NADA <nadaelsharkawy545@gmail.com>
2023-09-21 10:23:57 -0700

    g

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * _strlen - prints out the length of the str
+ * @s: str to print its size
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int counter;
+
+	counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+
+	return (counter);
+}
+
+/**
+ * add_node - dds a new node at the beginning of a list_t list
+ * @head: var
+ * @str: var
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	new->str = strdup(str);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+
+}
