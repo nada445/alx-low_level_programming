f98f815830951a8989fa5ef9e43f3460aa291394
NADA <nadaelsharkawy545@gmail.com>
2023-09-25 11:08:06 -0700

    task3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - ddddd
+ * @head: h
+ * @n: num
+ * Return: fevf
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *tmp = *head;
+
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (tmp->next)
+		tmp = tmp->next;
+
+	tmp->next = new;
+	return (new);
+}
