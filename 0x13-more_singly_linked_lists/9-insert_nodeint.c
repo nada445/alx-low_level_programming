76c6a21be68e4a610367fc2bb927bc3a1947f63f
NADA <nadaelsharkawy545@gmail.com>
2023-09-26 13:43:08 -0700

    s

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,33 +9,33 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *tmp, *new, *tmp2;
+	listint_t *tmp, *new;
 
 	new = malloc(sizeof(listint_t));
 
 	if (!new || !head || (!(*head) && idx > 0) || !new)
 		return (NULL);
-	tmp = *head;
-	while (tmp && i < idx - 1)
+	if (!idx)
 	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp)
+	{
+		if (i == idx - 1)
+		{
+			new->n = n;
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
 		tmp = tmp->next;
 		i++;
 	}
 
-	if (!tmp && i != idx - 1)
-	{
 		free(new);
 		return (NULL);
-	}
-	if  (!tmp && i == idx - 1)
-	{	free(new);
-		tmp->n = n;
-		tmp->next = NULL;
-		return (tmp);
-	}
-	tmp2 = tmp->next;
-	new->n = n;
-	new->next = tmp2;
-	tmp->next = new;
-	return (new);
 }
