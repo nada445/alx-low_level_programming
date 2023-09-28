d0218bdd929e854604021a44cce3ad643938810e
NADA <nadaelsharkawy545@gmail.com>
2023-09-28 13:32:43 -0700

    h

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_bit - ffjfjf
+ * @n: ffff
+ * @index: rurur
+ * Return: ef
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	return ((n >> index) & 1);
+}
