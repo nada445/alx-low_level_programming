97657c2fe22e56c1662f843d950612e29085f58b
NADA <nadaelsharkawy545@gmail.com>
2023-09-28 13:55:08 -0700

    g

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _pow - power fun
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+unsigned int _pow(int a, int b)
+{
+unsigned int result;
+int i;
+
+result = 1;
+
+for (i = 0; i < b; i++)
+{
+result *= a;
+}
+return (result);
+}
+
+/**
+ * clear_bit - gjbifkgb
+ * @n: fgbgyh
+ * @index: fghg
+ * Return: rtht
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	if (_pow(2, index) >  *n)
+		return (1);
+
+		*n = *n  - _pow(2, index);
+	return (1);
+}
