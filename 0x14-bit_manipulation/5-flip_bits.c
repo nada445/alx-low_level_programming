6799f99928f1f99d364bd0d89d97449b759104fa
NADA <nadaelsharkawy545@gmail.com>
2023-09-28 14:14:37 -0700

    l

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - t4gt
+ * @n: g
+ * @m: rtggt
+ * Return: rrtg
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int chack = n ^ m;
+	int cntr = 0;
+
+	while (chack)
+	{
+		if (chack & 1)
+			cntr++;
+		chack >>= 1;
+	}
+	return (cntr);
+
+}
+
