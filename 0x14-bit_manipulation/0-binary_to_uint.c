c449e62243afcc7bace1c684113ca186b72963ee
NADA <nadaelsharkawy545@gmail.com>
2023-09-28 12:06:33 -0700

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,46 @@
 #include "main.h"
 #include <string.h>
 /**
- * int binary_to_uint - mvvmvvv
+ * _strlen - prints out the length of the str
+ * @s: str to print its size
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int counter;
+
+	counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+
+	return (counter);
+}
+/**
+ * _pow - power fun
+ * @a: num1
+ * @b: num2
+ * Return: result
+ */
+int _pow(int a, int b)
+{
+int result;
+int i;
+
+result = 1;
+
+for (i = 0; i < b; i++)
+{
+result *= a;
+}
+return (result);
+}
+/**
+ * binary_to_uint - mvvmvvv
  * @b: nknjik
  * Return: jfjfj
  */
@@ -9,14 +48,21 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
 	int i;
-	int num = atoi(b);
+	int len = 0, pow = 0;
 
-	for (i = 0; num != 0; i++)
+	for (i = 0; b[i] != '\0'; i++)
+		len++;
+
+	if (!b)
+		return (0);
+
+	for (i = len - 1; i >= 0; i--)
 	{
-		dec += num % 10;
-		num /= 10;
+		if (b[i] > '9' || b[i] < '0')
+			return (0);
+		dec += (b[i] - 48) * _pow(2, pow);
+		pow++;
 	}
-	return (num);
+	return (dec);
 }
 
-		
