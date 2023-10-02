472f68de4aaa718c7933ba21aa280799b14471b1
NADA <nadaelsharkawy545@gmail.com>
2023-10-02 14:03:34 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	size_t j = 0;
 
 	if (!filename)
-		return (-2);
+		return (-1);
 	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
