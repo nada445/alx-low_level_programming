6a44622ff3a66d5a5c2dec779ad7b0b5183317e6
NADA <nadaelsharkawy545@gmail.com>
2023-10-02 13:51:11 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int len = _strlen(text_content);
 	int fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	int j;
+	int j = 0;
 
 	if (fp == -1)
 		return (-1);
