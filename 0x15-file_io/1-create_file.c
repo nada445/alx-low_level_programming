a29bd64d4869760331fbd710a0de06face698353
NADA <nadaelsharkawy545@gmail.com>
2023-10-02 14:18:10 -0700

    v

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	size_t len = _strlen(text_content);
+	int len = _strlen(text_content);
 	int fd;
 
 	if (!filename)
