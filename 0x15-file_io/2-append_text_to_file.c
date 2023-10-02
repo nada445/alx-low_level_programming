72d02bd9f4b33eb5fcc2d918b9734f0ef9366b51
NADA <nadaelsharkawy545@gmail.com>
2023-10-02 14:40:45 -0700

    v

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -36,7 +36,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fp = open(filename, | O_WRONLY | O_APPEND);
+	fp = open(filename, O_WRONLY | O_APPEND);
 
 	if (fp == -1)
 		return (-1);
